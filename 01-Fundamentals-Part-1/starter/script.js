32b7230da1c23c1b86e21bf196d7ae7ba5065529
Gogulan12 <gogulanravi12@gmail.com>
2022-10-30 19:11:23 -0400

    initial commit

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,159 @@
+// LECTURE: Values and Variables
+
+/*
+
+let js = "amazing";
+// if (js === "amazing") alert("Javascript is FUN!");
+
+console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+// declaring variable - store variable in computers memory
+let firstName = "Jonas";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matilda = "JM";
+let $function = 27;
+
+let person = "jonas";
+let PI = 3.1415;
+
+let myFirstJob = "Coder";
+let myCurrentJob = "Teacher";
+
+let job1 = "programmer";
+let job2 = "teacher;";
+// ^ Not good practice
+
+// variable is a box where we can store a value
+
+console.log(myFirstJob);
+
+// LECTURE: Values and Variables
+let country = "Canada";
+let continent = "North America";
+let population = 38000000;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+
+*/
+
+// LECTURE: Data Types
+
+/*
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof "Jonas");
+
+javascriptIsFun = "YES!";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
+// bug gives object but should be null
+
+// LECTURE: Data Types
+let isIsland = false;
+let language;
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
+*/
+
+// LECTURE: let, const and var
+
+/*
+let age = 30;
+age = 31;
+// changing variables also called mutating variable
+
+const birthYear = 1991;
+// birthYear = 1990;
+
+// const job;
+
+var job = "programmer";
+job = "teacher";
+
+// LECTURE: let, const and var
+const language = "English";
+*/
+
+// LECTURE: Basic Operators
+
+/*
+// Math operators
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2020;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 2, 2 ** 3);
+// 2 ** 3 meanse 2 to the power of 3 = 2 * 2 * 2
+
+const firstName = "Jonas";
+const lastName = "Schmedtmann";
+console.log(firstName + " " + lastName);
+
+// Assignment Operators
+let x = 10 + 5; // 15
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1
+x--; //
+x--; //
+console.log(x);
+
+// Comparion operators
+console.log(ageJonas > ageSarah); // >, <, >=, <=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
+
+// LECTURE: Basic Operators
+let country = "Canada";
+let continent = "North America";
+let population = 38000000 + 1;
+const language = "English";
+
+let halfPop = population / 2;
+console.log(halfPop);
+
+const finland = 6000000;
+const avgPop = 33000000;
+
+console.log(population >= finland);
+console.log(population <= avgPop);
+
+let description =
+  country +
+  " is in " +
+  continent +
+  ", and its " +
+  population +
+  " people speak " +
+  language;
+
+console.log(description);
+*/
